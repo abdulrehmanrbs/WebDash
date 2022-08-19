149dc889766da45c97fa747d1351bb85d7651c0d
abdulrehmanrbs <106783098+abdulrehmanrbs@users.noreply.github.com>
2022-08-19 08:29:01 -0400

    Graphs

--- app/app.py
+++ app/app.py
@@ -31,11 +31,6 @@ async def welcome(request: Request, db: Session=Depends(get_db)):
     px.defaults.width = 266
     px.defaults.height = 200
 
-    dfauthors = df['authors'].value_counts()
-    dfauthors = dfauthors.head(10)
-    dfjournal = df['journal'].value_counts()
-    dfjournal = dfjournal.head(10)
-    dfyear = df.sort_values('year',ascending=False).head(10)
     dft = df.groupby('authors').head(5).sort_values('year')
 
     fig1 = px.histogram(dft, x='authors', color='year')
@@ -52,12 +47,34 @@ async def welcome(request: Request, db: Session=Depends(get_db)):
         margin=dict(l=0, r=0, t=0, b=0))
     page2 = fig2.to_html(full_html=False, include_plotlyjs='cdn')
 
-    dft = df.groupby('authors').tail(5).sort_values('year')
-    fig3 = px.histogram(dft, x='authors', color='year')
+    fig3 = px.histogram(dft, x='authors', color='journal')
     fig3.update_layout(yaxis = dict(tickfont = dict(size=5)),
         xaxis = dict(tickfont = dict(size=5)),
         font=dict(size=5),
         margin=dict(l=0, r=0, t=0, b=0))
     page3 = fig3.to_html(full_html=False, include_plotlyjs='cdn')
 
-    return templates.TemplateResponse("chart.html", {"request":request, "page1":page1, "page2":page2, "page3":page3})
+    dft = df.groupby('authors').tail(5).sort_values('year')
+
+    fig4 = px.histogram(dft, x='authors', color='year')
+    fig4.update_layout(yaxis = dict(tickfont = dict(size=5)),
+        xaxis = dict(tickfont = dict(size=5)),
+        font=dict(size=5),
+        margin=dict(l=0, r=0, t=0, b=0))
+    page4 = fig4.to_html(full_html=False, include_plotlyjs='cdn')
+
+    fig5 = px.histogram(dft, x='year', color='journal')
+    fig5.update_layout(yaxis = dict(tickfont = dict(size=5)),
+        xaxis = dict(tickfont = dict(size=5)),
+        font=dict(size=5),
+        margin=dict(l=0, r=0, t=0, b=0))
+    page5 = fig5.to_html(full_html=False, include_plotlyjs='cdn')
+
+    fig6 = px.histogram(dft, x='authors', color='journal')
+    fig6.update_layout(yaxis = dict(tickfont = dict(size=5)),
+        xaxis = dict(tickfont = dict(size=5)),
+        font=dict(size=5),
+        margin=dict(l=0, r=0, t=0, b=0))
+    page6 = fig6.to_html(full_html=False, include_plotlyjs='cdn')
+
+    return templates.TemplateResponse("chart.html", {"request":request, "page1":page1, "page2":page2, "page3":page3, "page4":page4, "page5":page5, "page6":page6})
